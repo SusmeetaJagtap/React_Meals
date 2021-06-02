49ff0d420a05d9eed185aa16cc769e2ef6048b06
SusmeetaJ <sush.3996@gmail.com>
2021-06-02 10:23:49 +0530

    Added Reducer to maintain state

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContex from "./cart-contex";
-
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const updatedItems = state.items.concat(action.item);
+      const updatedAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedAmount,
+      };
+    case "REMOVE":
+  }
+};
+const cartInitialState = {
+  items: [],
+  totalAmount: 0,
+};
 const CartProvider = (props) => {
-  const addItemToCart = (item) => {};
-  const removeItemFromCart = (id) => {};
+  const [cartState, cartDispatchAction] = useReducer(
+    cartReducer,
+    cartInitialState
+  );
+  const addItemToCart = (item) => {
+    cartDispatchAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCart = (id) => {
+    cartDispatchAction({ type: "REMOVE", id: id });
+  };
   return (
     <CartContex.Provider
       value={{
-        cartItems: [],
-        totalAmount: 0,
+        cartItems: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCart,
         removeItem: removeItemFromCart,
       }}
